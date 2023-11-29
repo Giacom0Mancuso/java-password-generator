6cb215e4c54521fe9e6e01712d9d3c4f739d5149
Giacom0Mancuso <giacomomancuso35@email.com>
2023-11-29 14:57:20 +0100

    work done

---
+++ src/org/lessons/java/security/PasswordGeneretor.java
@@ -0,0 +1,24 @@
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGeneretor {
+    public static void main(String[] args) {
+        Scanner scanName = new Scanner(System.in);
+        String name = scanName.nextLine();
+        Scanner scanSurname = new Scanner(System.in);
+        String surname = scanSurname.nextLine();
+        Scanner scanFavColor = new Scanner(System.in);
+        String favColor = scanFavColor.nextLine();
+        Scanner scanDay = new Scanner(System.in);
+        int day = scanDay.nextInt();
+        Scanner scanMonth = new Scanner(System.in);
+        int month = scanMonth.nextInt();
+        Scanner scanYear = new Scanner(System.in);
+        int year = scanYear.nextInt();
+        String password;
+        password = name + surname + favColor +(day + month +year);
+        System.out.println(password);
+
+    }
+}
